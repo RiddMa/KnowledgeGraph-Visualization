570fe0035f426a626d770f6f7f4e5932912270fe
Ridd-Desktop-Ubuntu <riddma42@gmail.com>
2022-03-13 00:45:50 +0800

    init flask backend

---
+++ backend-java/src/main/java/com/riddma/cykg/CykgApplication.java
@@ -0,0 +1,13 @@
+package com.riddma.cykg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CykgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CykgApplication.class, args);
+	}
+
+}
